bb51f4c7fa1375c5461a775c72b50bd94a43775e
zhangbo1201 <13151443892@163.com>
2018-08-27 20:19:42 +0800

    策略模式

--- src/main/java/cn/edu/hhit/service/strategy/ContextStrategyFactory.java
+++ src/main/java/cn/edu/hhit/service/strategy/ContextStrategyFactory.java
@@ -3,7 +3,6 @@ package cn.edu.hhit.service.strategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -16,15 +15,7 @@ import java.util.Map;
 @Component
 public class ContextStrategyFactory {
     @Autowired
-    private Map<String, IContextStrategy> contextStrategy = new HashMap<String,IContextStrategy>();
-
-    public Map<String, IContextStrategy> getContextStrategy() {
-        return contextStrategy;
-    }
-
-    public void setContextStrategy(Map<String, IContextStrategy> contextStrategy) {
-        this.contextStrategy = contextStrategy;
-    }
+    private Map<String, IContextStrategy> contextStrategy;
 
     public IContextStrategy doStrategy(String type){
 
